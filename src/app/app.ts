ea5819b8e7ae5694b14060203798185ccf9a1db5
petesunday <piotrsadej@gmail.com>
2026-03-29 19:36:03 +0200

    feat: add change detection strategy to app component and update schematics in angular.json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'tp-root',
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('tuszpusz');
